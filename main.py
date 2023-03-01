c786bad11f6521819dc43252694fd46bafe47ef9
changiljeong <changil.jeong@tlgcohort.com>
2023-03-01 07:07:12 -0600

    added riddles function along with associated data

--- main.py
+++ main.py
@@ -18,22 +18,78 @@ def showStatus():
       print('You see a ' + rooms[currentRoom]['item'])
     print("---------------------------")
 inventory = []
+
+
+
+riddle_questions = {
+    "Who created Python?: ": "D",
+    "What year was the Python born?: ": "B",
+    "What statement/function does Python uses to display messages onto the screen?: ": "C",
+}
+
+options = [["A. Jeff Bezos", "B. Elon Musk", "C. Bill Gates", "D. Guido van Rossum"],
+           ["A. 1988", "B. 1991", "C. 2002", "D. 2010"],
+           ["A. console.log()", "B. System.out.println()", "C. print()", "D. println()"]]
+
+
+def riddles():
+
+    correct_guesses = 0
+    question_num = 1
+    for key, value in riddle_questions.items():
+        print("***********************************************************")
+        print(key)
+
+        for i in options[question_num-1]:
+            print(i)
+        guess = input("Select answers & type (A, B, C, or D): ")
+        guess = guess.upper()
+
+        if guess == value:
+            correct_guesses += 1
+            print(f"you got it! So far you correctly guessed: {correct_guesses}")            
+        else:
+            correct_guesses -= 1
+            print(f"WRONG! Deducting your score. So far you correctly guessed: {correct_guesses}")
+    
+        question_num += 1
+    if correct_guesses >= 3:
+        print("Good job, you guessed all 3 answers correctly. You can get the item and go north or south now.")
+    else:
+        print(f"You got {correct_guesses} numbers of questions wrong! Let's try again")
+        riddles()
+
+
+
+
 rooms = {
-            'Hall' : {
-                  'south' : 'Kitchen',
-                  'east'  : 'Dining Room',
-                  'item'  : 'key'                },
-            'Kitchen' : {
-                  'north' : 'Hall',
-                  'item'  : 'monster',
-                },
-            'Dining Room' : {
-                  'west' : 'Hall',
-                  'south': 'Garden',
-                  'item' : 'potion'               },
-            'Garden' : {
-                  'north' : 'Dining Room'            }
-         }
+
+    'Hall': {
+        'south': 'Kitchen',
+        'east': 'Dining Room',
+        'item': 'key'
+    },
+
+    'Kitchen': {
+        'north': 'Hall',
+        'item': 'monster',
+    },
+    'Dining Room': {
+        'west': 'Hall',
+        'south': 'Riddle Room',
+        'item': 'potion'
+    },
+    'Riddle Room': {
+        'north': 'Dining Room',
+        'item': 'star',
+        'south': 'Garden'
+    },
+    'Garden': {
+        'north': 'Riddle Room'
+    }
+}
+
+
 currentRoom = 'Hall'
 
 showInstructions()
@@ -59,6 +115,10 @@ while True:
             del rooms[currentRoom]['item']
         else:
              print('Can\'t get ' + move[1] + '!')
+
+    if currentRoom == 'Riddle Room':
+        print("Haha, you thought you can move around easily like that? You must successfully solve 3 random questions to move north or south! Good luck!")
+        riddles()
 
     if 'item' in rooms[currentRoom] and 'monster' in rooms[currentRoom]['item']:
         print('A monster has got you ... GAME OVER!')
